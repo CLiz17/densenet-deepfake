939890487708147c3e8c17d0ea1aaeed617ab6a5
CLiz17 <liz362002@gmail.com>
2024-03-24 12:30:00 +0530

    Add to save model weight

--- densenet.py
+++ densenet.py
@@ -91,6 +91,14 @@ model.compile(loss='binary_crossentropy',
               metrics=['accuracy'],
               optimizer=OPT)
 
+hist = model.fit(
+    train_generator,
+    epochs = 10,
+    validation_data = validation_generator
+)
+
+model.save('model_weights.h5')
+
 plt.plot(hist.history['accuracy'])
 plt.plot(hist.history['val_accuracy'])
 plt.title('Densenet Model Accuracy')
